cc6deb9be77b65506309ee788d6a9a01e11eef53
puyral <github@puyral.fr>
2026-07-08 07:25:54 +0200

    true init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "lspranto";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+    rust-flake.url = "github:juspay/rust-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (attrs: {
+      imports = [
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { self', ... }: {
+        devShells.default = self'.devShells.rust;
+
+        treefmt.programs = {
+          nixfmt.enable = true;
+          rustfmt.enable = true;
+        };
+      };
+
+      systems = [ "x86_64-linux" ];
+
+    });
+}
